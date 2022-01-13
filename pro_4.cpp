7986ab89e880a894c7ad4507a949934a101437fc
Keegan Goecke <kg333920@ohio.edu>
2022-01-13 16:12:39 -0500

    wrote main function

--- pro_4.cpp
+++ pro_4.cpp
@@ -8,12 +8,34 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 using namespace std;
 
+bool isPalin(int product);
+
 int main()
 {
+    int product;        // product of two 3-digit numbers
+    int highest = 0;    // hightest palindrome
     // find all possible products of two 3-digit numbers
+    for(int i = 100; i <= 999; i++)
+    {
+        for(int j = 100; i <= 999; j++)
+        {
+            product = i * j;            // gets product of i and j
+            if(isPalin(product))        // checks if product is a palindrome
+            {
+                if(product > highest)   // checks if product is higher than current high
+                {
+                    highest = product;  // updates current high
+                }
+            }
+        }
+    }
 
-    // find out if the product is a palindrome
-
-    // if palindrome is greater than current update new high
-    
+    cout << highest << endl;            // prints highest palindrome
 }
+
+// check if product is a palindrome
+bool isPalin(int product)
+{
+
+}
+
