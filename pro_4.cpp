adbb62a82bba2994e955ce9b8c939997eb1d5b12
kgoke <kg333920@ohio.ede>
2022-01-13 19:44:26 -0500

    Solution to problem #4 (added comments)

--- pro_4.cpp
+++ pro_4.cpp
@@ -11,41 +11,41 @@ using namespace std;
 bool ispalin(int n);
 
 int main(){
-  int n; 
-  int h = 0;
+  int n;        // product of two 3-digit numbers
+  int h = 0;    // highest palindrome
 
-  for(int i = 100; i < 1000; i++){
-      for(int j = 100; j < 1000; j++){
-          n = i * j;
-          if(ispalin(n)){
-              if(n > h)
+  for(int i = 100; i < 1000; i++){      // loops from 100 to 999
+      for(int j = 100; j < 1000; j++){  // loops from 100 to 999
+          n = i * j;                    // gets product of i and j
+          if(ispalin(n)){               // checks if n is a palindrome
+              if(n > h)                 // checks if palindrome is greater than current high
               {
-                  h = n;
+                  h = n;                // updates current high
               }
           }
       }
   }
 
-  cout << h << endl;
+  cout << h << endl;    // displays the highest palindrome 
    return 0;
 
 }
 
 bool ispalin(int n){
 
-    int rev = 0;
-    int rem;
-    int temp = n;
+    int rev = 0;    // reverse of input number
+    int rem;        // holder for remainder
+    int temp = n;   // tempary holder for input value
 
-    while(temp > 0){
-        rem = temp % 10;
-        temp = temp / 10;
-        rev = rev * 10 + rem;
+    while(temp > 0){            // loops while temp is greater than 0
+        rem = temp % 10;        // gets remainder of temp divided by 10
+        temp = temp / 10;       // divided temp by 10
+        rev = rev * 10 + rem;   // multiplies the reversed number by 10 and adds the remainder value
     }
 
-    if(rev == n)
-    return 1;
+    if(rev == n)    // checks if reverse number is equal to input number
+    return 1;       // returns true
     else
-    return 0;
+    return 0;       // returns false
 
 }
